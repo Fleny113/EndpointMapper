dc45633a8e09fe0e656b4b2230cee56428e89ceb
Fleny <fleny113@outlook.com>
2023-08-14 12:18:23 +0200

    Remove ConsoleApp partial class, Add interceptors

--- EndpointMapper.TestApplication.NativeAOT/Program.cs
+++ EndpointMapper.TestApplication.NativeAOT/Program.cs
@@ -1,4 +1,4 @@
-using ConsoleApp;
+using EndpointMapper;
 using EndpointMapper.TestApplication.NativeAOT;
 using System.Text.Json.Serialization;
 
@@ -9,23 +9,13 @@ builder.Services.ConfigureHttpJsonOptions(options =>
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
 
-builder.Services.AddSingleton("Hi World");
-
 var app = builder.Build();
 
-var todosApi = app.MapGroup("/todos");
-
-todosApi.MapEndpointMapperEndpoints();
+app
+    .MapGroup("/todos")
+    .MapEndpointMapperEndpoints();
 
 app.Run();
 
 [JsonSerializable(typeof(Todo[]))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext { }
-
-namespace ConsoleApp
-{
-    public static partial class Program
-    {
-        public static partial IEndpointRouteBuilder MapEndpointMapperEndpoints(this IEndpointRouteBuilder builder);
-    }
-}
