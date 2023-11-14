6de8113a6a9575e308d43cc0030c7ac45e67dc59
Fleny <fleny113@outlook.com>
2023-11-14 15:15:14 +0100

    Refactor .csproj

--- EndpointMapper.TestApplication/Endpoints/DependencyInjectionEndpoint.cs
+++ EndpointMapper.TestApplication/Endpoints/DependencyInjectionEndpoint.cs
@@ -5,9 +5,10 @@ using Microsoft.Extensions.Options;
 
 namespace EndpointMapper.TestApplication.Endpoints;
 
-public sealed class DependencyInjectionEndpoint : IEndpoint
+public sealed class DependencyInjectionEndpoint : IEndpoint, IConfigureEndpoint
 {
-    public void Configure(RouteHandlerBuilder builder) => builder.CacheOutput(x => x.Expire(TimeSpan.FromSeconds(10)));
+    public static void Configure(RouteHandlerBuilder builder, string route, string method) 
+        => builder.CacheOutput(x => x.Expire(TimeSpan.FromSeconds(10)));
 
     [HttpMap(HttpMapMethod.Get, "/di")]
     public static Ok<long> Handle([FromServices] IOptions<OutputCacheOptions> outputCachingOptions) 
