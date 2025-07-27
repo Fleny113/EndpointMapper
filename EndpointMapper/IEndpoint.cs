f6ac6a4f3a045897190005335ff7ebe79bdf509b
Fleny <fleny113@outlook.com>
2025-07-27 16:17:56 +0200

    Add Register and Configure to IEndpoint

--- EndpointMapper/IEndpoint.cs
+++ EndpointMapper/IEndpoint.cs
@@ -9,6 +9,31 @@ namespace EndpointMapper;
 /// </summary>
 public interface IEndpoint
 {
+    /// <summary>
+    /// Use the <see cref="IEndpointRouteBuilder"/> to map and configure the routes into the endpoint
+    /// </summary>
+    /// <remarks>
+    /// When using Trimming and NativeAOT, this is the preferred way to register your endpoints, as it allows the ASP.NET
+    /// Request Delegate Generator to generate the code to allow for Trimming as the code won't need Runtime Type Information
+    /// </remarks>
+    /// <param name="builder"><see cref="IEndpointRouteBuilder"/> to register and configure the route(s)</param>
+    public static virtual void Register(IEndpointRouteBuilder builder) { }
+
+    /// <summary>
+    /// Use the <see cref="RouteHandlerBuilder"/> to configure the endpoint created in the class
+    /// </summary>
+    /// <remarks>
+    /// Useful when want to configure something like the OutputCaching that accept an Action to don't
+    /// create a policy for each endpoint
+    /// </remarks>
+    /// <param name="builder">
+    /// The <see cref="RouteHandlerBuilder"/> to configure the route, same as concatenating methods on the result of
+    /// <see cref="EndpointRouteBuilderExtensions.MapGet(IEndpointRouteBuilder, string, RequestDelegate)"/>
+    /// for example
+    /// </param>
+    /// <param name="route">Route of the handler currently being configured</param>
+    /// <param name="method">HTTP Verbs of the handler currently being configured</param>
+    public static virtual void Configure(RouteHandlerBuilder builder, string route, string method) { }
 }
 
 /// <summary>
@@ -19,6 +44,10 @@ public interface IRegisterEndpoint
     /// <summary>
     /// Use the <see cref="IEndpointRouteBuilder"/> to map and configure the routes into the endpoint
     /// </summary>
+    /// <remarks>
+    /// When using Trimming and NativeAOT, this is the preferred way to register your endpoints, as it allows the ASP.NET
+    /// Request Delegate Generator to generate the code to allow for Trimming as the code won't need Runtime Type Information
+    /// </remarks>
     /// <param name="builder"><see cref="IEndpointRouteBuilder"/> to register and configure the route(s)</param>
     public static abstract void Register(IEndpointRouteBuilder builder);
 }
